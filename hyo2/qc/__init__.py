8ded5bc6f91bb5d48ff0ca945cf1413448ab6dab
giumas <giumas@yahoo.it>
2018-12-21 21:53:07 +0100

    minor change to author list

--- hyo2/qc/__init__.py
+++ hyo2/qc/__init__.py
@@ -8,7 +8,7 @@ logger.addHandler(logging.NullHandler())
 
 name = "QC"
 __version__ = '3.0.0'
-__author__ = 'gmasetti@ccom.unh.edu; tyanne.faulkes@noaa.gov; clinton.r.marcus@noaa.gov; brc@ccom.unh.edu'
+__author__ = 'gmasetti@ccom.unh.edu; tyanne.faulkes@noaa.gov; brc@ccom.unh.edu'
 __license__ = 'LGPLv3 license'
 __copyright__ = 'Copyright 2018 University of New Hampshire, Center for Coastal and Ocean Mapping'
 
