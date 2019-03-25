31128e5e71ee7c9e3c22ae29651d6b59caf249e9
giumas <giumas@yahoo.it>
2019-03-25 14:53:12 -0400

    updated example for FS v8

--- examples/survey/features/ex_feature_scan_v8.py
+++ examples/survey/features/ex_feature_scan_v8.py
@@ -1,4 +1,4 @@
-from PySide import QtGui
+from PySide2 import QtWidgets
 
 from hyo2.qc.common import default_logging
 import logging
@@ -6,13 +6,13 @@ import logging
 default_logging.load()
 logger = logging.getLogger()
 
-from hyo2.qc.qctools.qt_progress import QtProgress
+from hyo2.abc.app.qt_progress import QtProgress
 from hyo2.qc.survey.project import SurveyProject
 from hyo2.qc.common import testing
 from hyo2.qc.survey.scan.base_scan import survey_areas
 
-app = QtGui.QApplication([])
-wid = QtGui.QWidget()
+app = QtWidgets.QApplication([])
+wid = QtWidgets.QWidget()
 
 # create the project
 prj = SurveyProject(output_folder=testing.output_data_folder(), progress=QtProgress(parent=wid))
@@ -20,8 +20,8 @@ prj = SurveyProject(output_folder=testing.output_data_folder(), progress=QtProgr
 # add a S57 file
 s57_files = testing.input_test_files(".000")
 print("- S57 files: %d" % len(s57_files))
-# prj.add_to_s57_list(s57_files[3])
-prj.add_to_s57_list("C:/Users/gmasetti/Google Drive/QC Tools/data/survey/feature_scan/H12971_FFF_Field_2018.000")
+prj.add_to_s57_list(s57_files[3])
+# prj.add_to_s57_list("C:/Users/gmasetti/Google Drive/QC Tools/data/survey/feature_scan/H12971_FFF_Field_2018.000")
 # prj.add_to_s57_list("C:/Users/gmasetti/Google Drive/QC Tools/test data/2018_HSSD_Test/H12971_FFF_Office_2018Test.000")
 print("%s" % (prj.s57_list,))
 logger.debug(prj)
