136d18fca6fc75bdff5ab82998267afcaeae0722
giumas <giuseppemasetti@gmail.com>
2020-02-16 20:49:28 +0100

    updated CARIS support files to v4

--- hyo2/qc/__init__.py
+++ hyo2/qc/__init__.py
@@ -7,10 +7,10 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 name = "QC"
-__version__ = '3.1.5'
+__version__ = '3.1.6'
 __author__ = 'gmasetti@ccom.unh.edu; tyanne.faulkes@noaa.gov; julia.wallace@noaa.gov; brc@ccom.unh.edu'
 __license__ = 'LGPLv3 license'
-__copyright__ = 'Copyright 2019 University of New Hampshire, Center for Coastal and Ocean Mapping'
+__copyright__ = 'Copyright 2020 University of New Hampshire, Center for Coastal and Ocean Mapping'
 
 
 def hyo():
