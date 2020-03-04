825ed3fb7b88488ecdccf70bc3e950e2310b8f1e
giumas <giuseppemasetti@gmail.com>
2020-03-04 06:53:17 +0100

    added CATZOC tvu qc calculations

--- hyo2/qc/survey/gridqa/grid_qa_calc_v4.pyx
+++ hyo2/qc/survey/gridqa/grid_qa_calc_v4.pyx
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 import cython
 from libc.math cimport isnan
 
-
+#IHO TVU QC Calculations
 # noinspection PyUnresolvedReferences
 @cython.cdivision(False)
 @cython.boundscheck(False)
@@ -140,3 +140,371 @@ cpdef calc_tvu_qc_ff(float[:, :] bathy, float[:, :] product_uncertainty, float p
 
             else:
                 tvu_qc[r, c] = pr_unc_value / ((1. + (0.023 * bathy_value) ** 2) ** 0.5)
+
+
+# CATZOC A1 TVU CALCULATIONS
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_a1_dd(double[:, :] bathy, double[:, :] product_uncertainty, double pu_nodata, double[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float64_t bathy_value
+    cdef np.float64_t pr_unc_value
+    cdef np.float64_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (0.5 + (0.01 * bathy_value))
+
+
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_a1_df(double[:, :] bathy, float[:, :] product_uncertainty, float pu_nodata, double[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float64_t bathy_value
+    cdef np.float32_t pr_unc_value
+    cdef np.float64_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+                # logger.debug("is nan: %d %d" % (r, c))
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (0.5 + (0.01 * bathy_value))
+
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_a1_fd(float[:, :] bathy, double[:, :] product_uncertainty, double pu_nodata, float[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float32_t bathy_value
+    cdef np.float64_t pr_unc_value
+    cdef np.float32_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (0.5 + (0.01 * bathy_value))
+
+
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_a1_ff(float[:, :] bathy, float[:, :] product_uncertainty, float pu_nodata, float[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float32_t bathy_value
+    cdef np.float32_t pr_unc_value
+    cdef np.float32_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+                # logger.debug("is nan: %d %d" % (r, c)))
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (0.5 + (0.01 * bathy_value))
+
+
+
+# CATZOC A2/B TVU CALCULATIONS
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_a2b_dd(double[:, :] bathy, double[:, :] product_uncertainty, double pu_nodata, double[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float64_t bathy_value
+    cdef np.float64_t pr_unc_value
+    cdef np.float64_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (1.0 + (0.02 * bathy_value))
+
+
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_a2b_df(double[:, :] bathy, float[:, :] product_uncertainty, float pu_nodata, double[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float64_t bathy_value
+    cdef np.float32_t pr_unc_value
+    cdef np.float64_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+                # logger.debug("is nan: %d %d" % (r, c))
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (1.0 + (0.02 * bathy_value))
+
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_a2b_fd(float[:, :] bathy, double[:, :] product_uncertainty, double pu_nodata, float[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float32_t bathy_value
+    cdef np.float64_t pr_unc_value
+    cdef np.float32_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (1.0 + (0.02 * bathy_value))
+
+
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_a2b_ff(float[:, :] bathy, float[:, :] product_uncertainty, float pu_nodata, float[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float32_t bathy_value
+    cdef np.float32_t pr_unc_value
+    cdef np.float32_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+                # logger.debug("is nan: %d %d" % (r, c)))
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (1.0 + (0.02 * bathy_value))
+
+
+
+# CATZOC C TVU CALCULATIONS
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_c_dd(double[:, :] bathy, double[:, :] product_uncertainty, double pu_nodata, double[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float64_t bathy_value
+    cdef np.float64_t pr_unc_value
+    cdef np.float64_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (2.0 + (0.05 * bathy_value))
+
+
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_c_df(double[:, :] bathy, float[:, :] product_uncertainty, float pu_nodata, double[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float64_t bathy_value
+    cdef np.float32_t pr_unc_value
+    cdef np.float64_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+                # logger.debug("is nan: %d %d" % (r, c))
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (2.0 + (0.05 * bathy_value))
+
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_c_fd(float[:, :] bathy, double[:, :] product_uncertainty, double pu_nodata, float[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float32_t bathy_value
+    cdef np.float64_t pr_unc_value
+    cdef np.float32_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (2.0 + (0.05 * bathy_value))
+
+
+# noinspection PyUnresolvedReferences
+@cython.cdivision(False)
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+#@cython.profile(True)
+cpdef calc_tvu_qc_c_ff(float[:, :] bathy, float[:, :] product_uncertainty, float pu_nodata, float[:,:] tvu_qc):
+
+    cdef int rows = bathy.shape[0]
+    cdef int cols = bathy.shape[1]
+
+    cdef int r, c
+    cdef np.float32_t bathy_value
+    cdef np.float32_t pr_unc_value
+    cdef np.float32_t NaN = np.nan
+
+    for r in range(rows):
+
+        for c in range(cols):
+
+            bathy_value = bathy[r, c]
+            pr_unc_value = product_uncertainty[r, c]
+
+            if isnan(bathy_value) or (pr_unc_value == pu_nodata):
+                tvu_qc[r, c] = NaN
+                # logger.debug("is nan: %d %d" % (r, c)))
+
+            else:
+                tvu_qc[r, c] = pr_unc_value / (2.0 + (0.05 * bathy_value))
