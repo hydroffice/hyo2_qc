846d63b79ef5513ddef9d3a32309a54daa299c50
giumas <giuseppemasetti@gmail.com>
2023-07-02 12:38:01 +0200

    added PDF manual

--- docs/conf.py
+++ docs/conf.py
@@ -57,9 +57,9 @@ copyright = u'2023, CCOM/JHC,UNH'
 # built documents.
 #
 # The short X.Y version.
-version = '3.9'
+version = '3.10'
 # The full version, including alpha/beta/rc tags.
-release = '3.9.1'
+release = '3.10.0'
 
 numfig = True
 
